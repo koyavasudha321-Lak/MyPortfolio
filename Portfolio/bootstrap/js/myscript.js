065c6387f229b22b96f53333b0d7d8a62fc9b75a
koyavasudha321-Lak <71616728+koyavasudha321-Lak@users.noreply.github.com>
2021-06-25 22:46:56 +0530

    emailValidation,Password Validation,Retype Password Validation done!!!

--- Portfolio/bootstrap/js/myscript.js
+++ Portfolio/bootstrap/js/myscript.js
@@ -60,7 +60,7 @@ function email_validate(){
 
     if(email !== ''){
         if(pat.test(email)){
-            $('#email').css('border','3px solid green');
+            $('#email').css('border','2px solid green');
             $('#success').css('display','block');
             $('#error').css('display','none');
             $('#warning').css('display','none');
@@ -80,4 +80,70 @@ function email_validate(){
     }
 }
 
+/*pass strength*/
+function passFun(){
+    var e, l;
+    e = document.getElementById("txt");
+    l = document.getElementById("myInput1").value.length;
 
+    if(l == 0){
+        e.innerHTML = "";
+        document.getElementById("myInput1").style.border="";
+    }else if(l < 5){
+        e.innerHTML = "Weak Password";
+        e.style.color = "tomato";
+        document.getElementById("myInput1").style.border="2px solid red";
+    }else if(l < 10){
+        e.innerHTML = "Normal Password";
+        e.style.color = "#b8860b";
+        document.getElementById("myInput1").style.border="2px solid orange";
+    }else{
+        e.innerHTML = "Strong Password";
+        e.style.color = "#90ee90";
+        document.getElementById("myInput1").style.border="2px solid green";
+
+    }
+}
+
+/* confirm password*/
+function checkPass(){
+    let password = document.getElementById("myInput1").value;
+    let cfmPass = document.getElementById("myInput2").value;
+   
+    console.log(password,cfmPass);
+    let message = document.getElementById("txt1");
+    if(password.length != 0){
+        if(password == cfmPass){
+            message.textContent = "passwords matched";
+            message.style.backgroundColor = "green";
+            $('#hide8').css('display','none');
+            $('#hide9').css('display','block');
+        }else{
+            message.textContent = "passwords should match";
+            message.style.backgroundColor = "crimson";
+            $('#hide8').css('display','block');
+            $('#hide9').css('display','none');
+        }
+    }
+}
+/*login form validation*/
+function login_form_validate(){
+    let ema = document.getElementById('em_er').value;
+    let pasw = document.getElementById('myInput').value;
+    if(ema ==""){
+        alert('Please Enter your Valid Email');
+        document.getElementById('em_er').style.borderColor = "red";
+
+
+    }
+    if(pasw == ""){
+        alert('Please Enter your Valid password');
+        document.getElementById('myInput').style.borderColor = "orange";
+    }
+   
+    
+}
+
+
+
+    
